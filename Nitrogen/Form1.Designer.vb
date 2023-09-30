94129a6caf78dd29328aa942f0de87f21338d8ba
m1ke10 <bitmikegaming@gmail.com>
2023-09-30 11:59:09 -0400

    adding vb project to repository.

---
+++ Nitrogen/Form1.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox2.BackgroundImage = CType(resources.GetObject("PictureBox2.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(1, -2)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(395, 83)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(1, 87)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(395, 58)
+        Me.RichTextBox1.TabIndex = 4
+        Me.RichTextBox1.Text = ""
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(138, 151)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(113, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Generate my key"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(395, 177)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "N7 Keygen"
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Nitrogen/Form1.vb
@@ -0,0 +1,81 @@
+﻿Imports System.Security.Cryptography
+Imports System.Text
+Imports System.Net.NetworkInformation
+Imports System.Media
+Imports System.Threading
+Public Class Form1
+    Dim player As New SoundPlayer(My.Resources.youtube_LDRNYUucKvA_audio)
+    Dim playbackThread As Thread
+
+    Private Function GetMacAddress() As String
+        Dim networkInterfaces() As NetworkInterface = NetworkInterface.GetAllNetworkInterfaces()
+        For Each netInterface As NetworkInterface In networkInterfaces
+            If (netInterface.NetworkInterfaceType = NetworkInterfaceType.Ethernet OrElse
+            netInterface.NetworkInterfaceType = NetworkInterfaceType.Wireless80211) AndAlso
+            netInterface.OperationalStatus = OperationalStatus.Up Then
+                Return netInterface.GetPhysicalAddress().ToString()
+            End If
+        Next
+
+        Return Nothing
+    End Function
+
+
+    Public Sub DisplayHashedMacAddress()
+        Try
+            Dim macAddress As String = GetMacAddress()
+
+            If macAddress IsNot Nothing Then
+                Dim sha256 As SHA256 = SHA256.Create()
+                Dim data As Byte() = Encoding.Default.GetBytes(macAddress)
+                Dim hashedBytes As Byte() = sha256.ComputeHash(data)
+                Dim sb As New StringBuilder()
+
+                For Each b As Byte In hashedBytes
+                    sb.Append(b.ToString("x2")) ' Convert bytes to hexadecimal representation
+                Next
+
+                RichTextBox1.Text = sb.ToString()
+                Refresh()
+            Else
+                RichTextBox1.Text = "MAC address not available."
+                Refresh()
+            End If
+        Catch ex As Exception
+            RichTextBox1.Text = "Error: " & ex.Message
+            Refresh()
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        playbackThread = New Thread(AddressOf PlayBackgroundMusic)
+        playbackThread.IsBackground = True
+        playbackThread.Start()
+    End Sub
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        ' Stop the background thread before closing the form
+        playbackThread.Abort() ' Terminate the background thread (not recommended for production use)
+    End Sub
+
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+    Private Sub PlayBackgroundMusic()
+        ' Continuously play the audio in a loop
+        Do
+            player.PlaySync() ' Use PlaySync to block and play continuously
+        Loop
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        DisplayHashedMacAddress()
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub RichTextBox2_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
